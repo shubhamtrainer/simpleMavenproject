441372787a89c6b7aded90a372f7e66d83a2f56a
QSP <QSP@DESKTOP-BKE9C99>
2024-01-03 12:35:13 +0530

    new one

---
+++ simpleMaven/src/test/java/shopping/TestyssTest.java
@@ -0,0 +1,14 @@
+package shopping;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestyssTest {
+	
+	@Test
+	public void demo()
+	{
+		Reporter.log("jenkins tested",true);
+	}
+
+}
